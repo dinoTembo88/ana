eb25bc3a7f4c6fa35e49567d19efc8501d2d4a6b
Dino Kraljević <dino.kralevic@gmail.com>
2011-05-15 20:19:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ana::Application.config.secret_token = '4a041dc43c2badefa08e694ffef7d3b4ecbb66a3c9f60ef6e9b2926b3bb0816881b2cf08bd43950e87721b659783f231bfdc8d2e646e45467be099fdfc5906f6'
